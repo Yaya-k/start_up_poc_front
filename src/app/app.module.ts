46f373d4ed50fd916adc958ebd39999e7779cf06
Yayak <yaya.kamissokho@gmail.com>
2021-02-11 12:29:54 +0100

    update readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InscriptionComponent } from './inscription/inscription.component';
-import { ConnexionComponent } from './connexion/connexion.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { LogInComponent } from './components/log-in/log-in.component';
@@ -16,12 +14,6 @@ import {HttpClientModule} from '@angular/common/http';
 import {RestapiService} from './restapi.service';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { ManangerDashboardComponent } from './components/mananger-dashboard/mananger-dashboard.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LayoutsComponent } from './layouts/layouts.component';
-import { AdvanceComponent } from './components/advance/advance.component';
-import { BasicComponent } from './components/basic/basic.component';
-import { FormsComponent } from './components/forms/forms.component';
-import { TablesComponent } from './components/tables/tables.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
@@ -29,19 +21,11 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
-    InscriptionComponent,
-    ConnexionComponent,
     LogInComponent,
     RegisterComponent,
     UserHomePageComponent,
     AdminDashboardComponent,
     ManangerDashboardComponent,
-    DashboardComponent,
-    LayoutsComponent,
-    AdvanceComponent,
-    BasicComponent,
-    FormsComponent,
-    TablesComponent
   ],
   imports: [
     FormsModule,
